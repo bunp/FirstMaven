65a199725e85e08e37687c6807233458f06053b3
yuheng <18809424238@163.com>
2022-07-20 03:24:34 +0800

    master update

--- src/main/java/com/self/maven/App.java
+++ src/main/java/com/self/maven/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("dev Update");
         System.out.println("devUpdate2");
         System.out.println("masterUpdate");
+        System.out.println("push");
     }
 }
