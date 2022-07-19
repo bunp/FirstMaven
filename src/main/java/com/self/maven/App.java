4057e8b56f8eed617769b957284421bf240b0cc1
yuheng <18809424238@163.com>
2022-07-20 02:41:30 +0800

    hhh

--- src/main/java/com/self/maven/App.java
+++ src/main/java/com/self/maven/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("update");
         System.out.println("fdsfsdds");
         System.out.println( "Hello World!" );
     }
