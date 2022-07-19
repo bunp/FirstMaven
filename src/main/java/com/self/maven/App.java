8e9d3f7c814eac9a26e3ee94d923d215baeb8f7c
yuheng <30950776+bunp@users.noreply.github.com>
2022-07-20 03:30:00 +0800

    updATE

--- src/main/java/com/self/maven/App.java
+++ src/main/java/com/self/maven/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println("devUpdate2");
         System.out.println("masterUpdate");
         System.out.println("push");
+        System.out.println("updATE");
     }
 }
