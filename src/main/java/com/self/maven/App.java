709447f09040d561f4f112e9962b22da414e56f9
yuheng <18809424238@163.com>
2022-07-20 02:38:37 +0800

    hhh

---
+++ src/main/java/com/self/maven/App.java
@@ -0,0 +1,14 @@
+package com.self.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("fdsfsdds");
+        System.out.println( "Hello World!" );
+    }
+}
